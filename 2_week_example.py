e560b9e098ddb488ad2e9599eaa029d6c7a25588
tintoll <tintoll@tintollui-MacBook-Pro.local>
2017-08-30 23:39:09 +0900

    2week study

---
+++ 2_week_example.py
@@ -0,0 +1,27 @@
+# 기본 자료형
+a = 1
+b = 1
+print(a, b)
+a = 1.5
+b = 3.5
+print(a, b)
+a = "ddd"
+b = 'sss'
+print(a, b)
+b = True
+a = False
+print(a, b)
+
+# 제곱승 (**)
+print(3 * 3 * 3 * 3 * 3) # 243
+print(3 ** 5) # 243
+
+# 형변환
+a = float(10)
+print(a)    # 10.0
+print(int(5.9)) # 5
+a = '76.3'  # string
+b = float(a) # float
+c = str(b) # string
+# 타입 확인할때
+type(a) # <class string>
